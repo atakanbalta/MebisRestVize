2fb4cadb8de2ca7ef3090426fb005d6959ddfaf9
atakanbalta <atakansahinbalta13@gmail.com>
2020-04-19 17:02:06 +0300

    Proje oluşturuldu

---
+++ MebisRestServisVize/src/main/java/tr/edu/medipol/mebis/MebisRestServisVizeApplication.java
@@ -0,0 +1,14 @@
+package tr.edu.medipol.mebis;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MebisRestServisVizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MebisRestServisVizeApplication.class, args);
+	}
+
+}
